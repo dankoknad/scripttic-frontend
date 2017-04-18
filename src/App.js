c9f5a5654784b82a59c32d17271b4a58492a4217
dankoknad <dankomilutinovic@gmail.com>
2017-04-18 14:11:55 +0200

    added bootstrap's stylesheet and markup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,11 +9,15 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Hello Scripttic</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+					<div className="row">
+						<div className="col-sm-4">col no 01</div>
+						<div className="col-sm-4">col no 02</div>
+						<div className="col-sm-4">col no 03</div>
+					</div>
+				</div>
       </div>
     );
   }
