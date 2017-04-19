0cd1ac93cd3aef6976b180bed6cae8864df43202
dankoknad <dankomilutinovic@gmail.com>
2017-04-19 06:56:12 +0200

    load articles titles on homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+
+import {
+	loadArticles,
+	baseUrl
+} from './lib/helpers.js';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import logo from './logo.svg';
 import './App.css';
 import './css/index.css';
 
 class App extends Component {
+	state = {
+		articles: []
+	}
+
+	componentDidMount(){
+    loadArticles(baseUrl)
+      .then(data => {
+			  const ordered =	_.orderBy(data, (o) => o.id, 'desc' );
+				this.setState({articles: ordered})
+			})    
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,7 +33,13 @@ class App extends Component {
         </div>
         <div className="container">
 					<div className="row">
-						<div className="col-sm-4">col no 01</div>
+						<div className="col-sm-4">
+							<h3>Articles:</h3>
+							{this.state.articles.length
+								? this.state.articles.map(article => <div key={article.id}>{article.title}</div>)
+								: null
+							}
+						</div>
 						<div className="col-sm-4">col no 02</div>
 						<div className="col-sm-4">col no 03</div>
 					</div>
---
+++ src/lib/helpers.js
@@ -0,0 +1,70 @@
+// import Remarkable from 'remarkable';
+export const baseUrl = 'http://www.scripttic.com:8000/api/v1/article';
+
+
+// load data
+export const loadArticles = (url) => {
+  return fetch(url)
+    .then(res => res.json())
+}
+
+// save data addPost
+export const publishPost = (post, url) => {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(post)
+  }).then(res => res.json())
+}
+
+// save data updatePost
+export const updatePost = (url, id, post) => {
+  return fetch(`${url}/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(post)
+  }).then(res => res.json())
+}
+
+// update data updateEmployee 
+export const updateEmployee = (employee) => {
+  return fetch(`${baseUrl}/${employee.id}`, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(employee)
+  }).then(res => res.json())
+}
+
+// update data removePost
+export const removePost = (url, id) => {
+  return fetch(`${url}/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+}
+
+// render markdown
+// export const renderMarkdown = (txt) => {
+// 	const md = new Remarkable({breaks: true});
+// 	return { __html: md.render(txt) };
+// }
+
+// generate timestamp
+export const getTimeStamp = () => {
+	return  new Date();
+}
+
+// timestamp to readable
+export const getDateFromTimestamp = (timeStamp) => new Date(+ timeStamp).toUTCString();
