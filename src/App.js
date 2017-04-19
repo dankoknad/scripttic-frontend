36f28f0bcd0b9ce956f4ea69c8b09137bf3ba35e
dankoknad <dankomilutinovic@gmail.com>
2017-04-19 23:12:18 +0200

    added responsive main navigation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 
 import ArticlesLinks from './ArticlesLinks';
 import Article from './Article';
@@ -52,6 +54,28 @@ class App extends Component {
 						<img src={logo} className="App-logo" alt="logo" />
 						<h3>Hello Scripttic</h3>
 					</div>
+					<Navbar inverse collapseOnSelect>
+						<Navbar.Header>
+							<Navbar.Toggle />
+						</Navbar.Header>
+						<Navbar.Collapse>
+							<Nav pullRight>
+								<LinkContainer to="/" exact activeClassName="active">
+									<NavItem eventKey={2} href="#">Articles</NavItem>
+								</LinkContainer>
+								<LinkContainer to="/login" activeClassName="active">
+									<NavItem eventKey={2} href="#">Login</NavItem>
+								</LinkContainer>
+								<NavDropdown eventKey={3} title="Dropdown to nowhere" id="basic-nav-dropdown">
+									<MenuItem eventKey={3.1}>Action</MenuItem>
+									<MenuItem eventKey={3.2}>Another action</MenuItem>
+									<MenuItem eventKey={3.3}>Something else here</MenuItem>
+									<MenuItem divider />
+									<MenuItem eventKey={3.4}>Separated link</MenuItem>
+								</NavDropdown>
+							</Nav>
+						</Navbar.Collapse>	
+					</Navbar>
 					<div className="container">
 						<div className="row">
 							<div className="col-sm-12">
@@ -71,6 +95,11 @@ class App extends Component {
 										}} />
 									: null
 								}
+
+								<Route path="/login" render={() => (
+                	<div>Login page</div>
+									)}
+								/>
 
 							</div>
 						</div>
--- src/Article.jsx
+++ src/Article.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 const Article = ({article, children}) => {
 	return (
 		<div>	
-			<h3>
-				<Link to="/">
-					<span style={{verticalAlign: 'text-top'}} className="glyphicon glyphicon-menu-left" aria-hidden="true" />
-					All Articles
-				 </Link>
-			</h3>
 			<h3>{article.title}</h3>		
 			<p>{article.body}</p>
 			<code>article ID: {article.id}</code>
