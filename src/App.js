b8c264909de594aa5bcc2e04f13b46b2ac00ff56
dankoknad <dankomilutinovic@gmail.com>
2017-04-24 15:25:57 +0200

    added animation on remove coments via React's ReactCSSTransitionGroup

--- src/App.js
+++ src/App.js
@@ -235,7 +235,7 @@ class App extends Component {
 	handleCommentRemove = (e, comment) => {
 		e.preventDefault();
 		const {token, comments} = this.state;
-		const commentIndex = _.findIndex(comments, function(o) { return o.id == comment.id; });
+		const commentIndex = _.findIndex(comments, function(o) { return o.id === comment.id; });
 		
 		// console.log(comment.article, comment.id);
 		// console.log(commentIndex);
--- src/pages/admin/MyComments.jsx
+++ src/pages/admin/MyComments.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 const MyComments = ({comments, handleCommentRemove}) => {
 	return (
@@ -6,18 +7,24 @@ const MyComments = ({comments, handleCommentRemove}) => {
 			{(comments.length) ? <h3>My comments:</h3> : null}
 			{(comments.length)
 				? <div className="list-group">
-						{comments.map(comment => (
-							<div key={comment.id} className="list-group-item">
-								<div>{comment.title}</div>
-								<div>{comment.body}</div>
-								<div>id <code>{comment.id}</code></div>
-								<div>poster <code>{comment.poster}</code></div>
-								<div>article <code>{comment.article}</code></div>
-								<div className="text-right">
-									<a onClick={(e) => handleCommentRemove(e, comment)} href="#" className="text-danger glyphicon glyphicon-remove"></a>
+						<ReactCSSTransitionGroup
+								component="div"
+								transitionName="list-anim"
+								transitionEnterTimeout={150}
+								transitionLeaveTimeout={250}>
+							{comments.map(comment => (
+								<div  key={comment.id} className="list-group-item">
+									<div>{comment.title}</div>
+									<div>{comment.body}</div>
+									<div>id <code>{comment.id}</code></div>
+									<div>poster <code>{comment.poster}</code></div>
+									<div>article <code>{comment.article}</code></div>
+									<div className="text-right">
+										<a onClick={(e) => handleCommentRemove(e, comment)} href="#" className="text-danger glyphicon glyphicon-remove"></a>
+									</div>
 								</div>
-							</div>
-						))}
+							))}
+						</ReactCSSTransitionGroup>
 					</div>
 				: <h3>you don't have any comments yet</h3>
 			}
